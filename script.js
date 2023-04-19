8c47a8b3b6d104e8ebdd9eab95a269e535725e6e
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-19 17:37:03 +0200

    add while cycle for list

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ const arrList = [
     'acqua',
     'pesce'
 ]
+
+const eleList = document.getElementById('#list');
+
+i = 0 ;
+
+while (i < arrList.lenght) {
+    console.log(arrList[i]);
+    eleList.innerHTML += `<li>${arrList[i]}</li>`;
+    i++
+}
